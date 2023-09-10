13c3a531bc9499841eec30707fa918ce9e78ecaf
Tahsan Zahid <112720232+TahsanX@users.noreply.github.com>
2023-09-10 16:09:40 +0600

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,23 @@
+let ol = document.querySelector("ol");
+let input = document.querySelector("input");
+let button = document.querySelector("button");
+button.addEventListener("click",function(){
+    if (input.value!="") {
+        let li = document.createElement("li");
+        let dlt = document.createElement("button");
+        dlt.innerText = "Delete";
+        dlt.addEventListener("click",function(event){
+            console.log(event.target.tagName);
+            // dlt.parentElement.style.display = "none";
+            dlt.parentElement.remove();
+        })
+        li.innerText = input.value;
+        ol.appendChild(li);
+        li.appendChild(dlt);
+    }
+    input.value = "";
+});
+
+// this dile tag ta or jinish ja chilo oita trigger hoy
+// event dile ki kaj hoise oita trigger hoy
+
